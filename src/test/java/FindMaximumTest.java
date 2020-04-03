ae2e6771f493ae63a0cf09108ff342f2804763a9
prithvi0 <prithviy96@gmail.com>
2020-04-04 03:29:42 +0530

    Added testcase to ensure max number is at 3rd position

--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -16,4 +16,11 @@ public class FindMaximumTest {
         Integer findMaxInteger = maximum.findMaximumInteger(new Integer[]{3, 33, 4});
         Assert.assertEquals(33, (int) findMaxInteger);
     }
+    // T.C 1.3: TEST CASE TO ENSURE MAX NUMBER IS IN THE THIRD POSITION
+    @Test
+    public void givenMaximumIntegerNumber_whenAtThirdPosition_shouldReturnSameNumber() {
+        FindMaximum maximum = new FindMaximum();
+        Integer findMaxInteger = maximum.findMaximumInteger(new Integer[]{3, 4, 33});
+        Assert.assertEquals(33, (int) findMaxInteger);
+    }
 }
