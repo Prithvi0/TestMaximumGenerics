b4a838e1d9170ee0322a2f8ca391ac5797d9b7f2
prithvi0 <prithviy96@gmail.com>
2020-04-04 03:13:03 +0530

    Refactored code in main file to return correct maximum integer position

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,9 +1,13 @@
 public class FindMaximum {
-    public Integer findMaximumInteger(Integer[] maximumInteger) {
-        Integer maximum = maximumInteger[0];
-        for (int max = 0; max < maximumInteger.length; max++)
-            if (maximum.compareTo(maximum) > 0)
-                maximum = max;
-            return maximum;     //  FIND THE MAXIMUM INTEGER FROM GIVEN INTEGERS
+    // METHOD TO FIND THE MAXIMUM INTEGER
+    public Integer findMaximumInteger(Integer[] integers) {
+        Integer maximumInteger = integers[0];
+        if (maximumInteger.compareTo(integers[1]) < 0){
+            maximumInteger = integers[1];
+        }
+        if (maximumInteger.compareTo(integers[2]) < 0){
+            maximumInteger = integers[2];
+        }
+        return maximumInteger;
     }
 }
--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -4,15 +4,9 @@ import org.junit.Test;
 public class FindMaximumTest {
     //  T.C 1.1: TEST CASE TO ENSURE MAX NUMBER IS IN THE FIRST POSITION
     @Test
-    public void givenMaximumAtPosition1_ShouldReturnTheSame() {
-        FindMaximum maximum = new FindMaximum();
-        Integer findMaxInteger = maximum.findMaximumInteger(new Integer[]{33, 3, 4});
-        Assert.assertEquals(33, (int) findMaxInteger);
-    }
-    @Test
-    public void givenMaximumAtAnotherPosition_ShouldNotBeEqualAndPassTestCase() {
+    public void givenMaximumIntegerNumber_whenAtFirstPosition_shouldReturnSameNumber() {
         FindMaximum maximum = new FindMaximum();
         Integer findMaxInteger = maximum.findMaximumInteger(new Integer[]{3, 33, 4});
-        Assert.assertNotEquals(33, (int) findMaxInteger);
+        Assert.assertEquals(33, (int) findMaxInteger);
     }
 }
