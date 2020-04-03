0188e7d455f679b549c830e2495b295415c0478c
prithvi0 <prithviy96@gmail.com>
2020-04-04 03:27:32 +0530

    Added testcase to ensure max number is at 2nd position

--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -4,9 +4,16 @@ import org.junit.Test;
 public class FindMaximumTest {
     //  T.C 1.1: TEST CASE TO ENSURE MAX NUMBER IS IN THE FIRST POSITION
     @Test
-    public void givenMaximumIntegerNumber_whenAtFirstPosition_shouldReturnSameNumber() {
+    public void givenMaximumIntegerNumber_whenAtFirstPosition_ShouldReturnSameNumber() {
         FindMaximum maximum = new FindMaximum();
         Integer findMaxInteger = maximum.findMaximumInteger(new Integer[]{33, 3, 4});
+        Assert.assertEquals(33, (int) findMaxInteger);
+    }
+    // T.C 1.2: TEST CASE TO ENSURE MAX NUMBER IS IN THE SECOND POSITION
+    @Test
+    public void givenMaximumIntegerNumber_whenAtSecondPosition_ShouldReturnSameNumber() {
+        FindMaximum maximum = new FindMaximum();
+        Integer findMaxInteger = maximum.findMaximumInteger(new Integer[]{3, 33, 4});
         Assert.assertEquals(33, (int) findMaxInteger);
     }
 }
