169168da3f30efe33cda97c62fd5e98a52273528
prithvi0 <prithviy96@gmail.com>
2020-04-04 21:58:07 +0530

    Added testcases to return maximum float numbers

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -2,12 +2,23 @@ public class FindMaximum {
     // METHOD TO FIND THE MAXIMUM INTEGER
     public Integer findMaximumInteger(Integer[] integers) {
         Integer maximumInteger = integers[0];
-        if (maximumInteger.compareTo(integers[1]) < 0){
+        if (maximumInteger.compareTo(integers[1]) < 0) {
             maximumInteger = integers[1];
         }
-        if (maximumInteger.compareTo(integers[2]) < 0){
+        if (maximumInteger.compareTo(integers[2]) < 0) {
             maximumInteger = integers[2];
         }
         return maximumInteger;
+    }
+    // METHOD TO FIND THE MAXIMUM FLOAT NUMBER
+    public Float findMaximumFloat(Float[] floats) {
+        Float maximumFloat = floats[0];
+        if (maximumFloat.compareTo(floats[1]) < 0.0f) {
+            maximumFloat = floats[1];
+        }
+        if (maximumFloat.compareTo(floats[2]) < 0.0f) {
+            maximumFloat = floats[2];
+        }
+        return maximumFloat;
     }
 }
--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -2,25 +2,41 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class FindMaximumTest {
+    FindMaximum maximum = new FindMaximum();
     //  T.C 1.1: TEST CASE TO ENSURE MAX NUMBER IS IN THE FIRST POSITION
     @Test
-    public void givenMaximumIntegerNumber_whenAtFirstPosition_ShouldReturnSameNumber() {
-        FindMaximum maximum = new FindMaximum();
+    public void givenMaximumIntegerNumber_WhenAtFirstPosition_ShouldReturnSameNumber() {
         Integer findMaxInteger = maximum.findMaximumInteger(new Integer[]{33, 3, 4});
         Assert.assertEquals(33, (int) findMaxInteger);
     }
     // T.C 1.2: TEST CASE TO ENSURE MAX NUMBER IS IN THE SECOND POSITION
     @Test
-    public void givenMaximumIntegerNumber_whenAtSecondPosition_ShouldReturnSameNumber() {
-        FindMaximum maximum = new FindMaximum();
+    public void givenMaximumIntegerNumber_WhenAtSecondPosition_ShouldReturnSameNumber() {
         Integer findMaxInteger = maximum.findMaximumInteger(new Integer[]{3, 33, 4});
         Assert.assertEquals(33, (int) findMaxInteger);
     }
     // T.C 1.3: TEST CASE TO ENSURE MAX NUMBER IS IN THE THIRD POSITION
     @Test
-    public void givenMaximumIntegerNumber_whenAtThirdPosition_shouldReturnSameNumber() {
-        FindMaximum maximum = new FindMaximum();
+    public void givenMaximumIntegerNumber_WhenAtThirdPosition_ShouldReturnSameNumber() {
         Integer findMaxInteger = maximum.findMaximumInteger(new Integer[]{3, 4, 33});
         Assert.assertEquals(33, (int) findMaxInteger);
+    }
+    // T.C 2.1 TEST CASE TO ENSURE MAX FLOAT NUMBER IS IN THE FIRST POSITION
+    @Test
+    public void givenMaximumFloatNumber_WhenAtFirstPosition_ShouldReturnSameNumber() {
+        Float findMaxFloatNumber = maximum.findMaximumFloat(new Float[]{3.1f, 1.1f, 2.1f});
+        Assert.assertEquals((Float) 3.1f, findMaxFloatNumber);
+    }
+    // T.C 2.2 TEST CASE TO ENSURE MAX FLOAT NUMBER IS IN THE SECOND POSITION
+    @Test
+    public void givenMaximumFloatNumber_WhenAtSecondPosition_ShouldReturnSameNumber() {
+        Float findMaxFloatNumber = maximum.findMaximumFloat(new Float[]{1.1f, 3.1f, 2.1f});
+        Assert.assertEquals((Float) 3.1f, findMaxFloatNumber);
+    }
+    // T.C 2.3 TEST CASE TO ENSURE MAX FLOAT NUMBER IS IN THE THIRD POSITION
+    @Test
+    public void givenMaximumFloatNumber_WhenAtThirdPosition_ShouldReturnSameNumber() {
+        Float findMaxFloatNumber = maximum.findMaximumFloat(new Float[]{1.1f, 2.1f, 3.1f});
+        Assert.assertEquals((Float) 3.1f, findMaxFloatNumber);
     }
 }
