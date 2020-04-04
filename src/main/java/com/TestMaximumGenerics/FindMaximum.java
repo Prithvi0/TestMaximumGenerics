8cdf7c451896c7685a0f2048f9ae5c2ae87cd150
prithvi0 <prithviy96@gmail.com>
2020-04-05 05:03:40 +0530

    Refactored class to generic type & created a separate testcase

--- src/main/java/com/TestMaximumGenerics/FindMaximum.java
+++ src/main/java/com/TestMaximumGenerics/FindMaximum.java
@@ -1,16 +1,28 @@
 package com.TestMaximumGenerics;
 
-public class FindMaximum {
-    @SuppressWarnings("unchecked")
+//  GENERIC CLASS CREATION
+@SuppressWarnings("unchecked")  //  ANNOTATION TO IGNORE WARNING GENERATED DUE TO UNCHECKED CASTING
+public class FindMaximum<T extends Comparable<T>> {
+    T firstValue, secondValue, thirdValue;
+    //  PARAMETER CONSTRUCTOR
+    public FindMaximum(T firstValue, T secondValue, T thirdValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
+    }
+    // METHOD TO CALL INTERNALLY BY PASSING 3 INSTANCE VARIABLES
+    public T testMaximum() {
+        return (T) FindMaximum.testMaximum(firstValue, secondValue, thirdValue);
+    }
     // GENERIC METHOD TO FIND MAXIMUM VALUE
-    public static <T extends Comparable> T maximumValue (T[] max) {
-        T maximum = max[0];
-        if (maximum.compareTo(max[1]) < 0) {
-            maximum = max[1];
+    public static <T extends Comparable<T>> T testMaximum(T firstValue, T secondValue, T thirdValue) {
+        T max = firstValue; //  INITIALLY, ASSUMING IT AS THE LARGEST VALUE
+        if (secondValue.compareTo(max) > 0) {
+            max = secondValue;  // IF CONDITION SATISFIES, ASSUMES IT AS THE LARGEST VALUE
         }
-        if (maximum.compareTo(max[2]) < 0) {
-            maximum = max[2];
+        if (thirdValue.compareTo(max) > 0) {
+            max = thirdValue;   //  IF CONDITION SATISFIES, RETURNS THIS AS THE LARGEST VALUE
         }
-        return maximum;
+        return max;
     }
 }
--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -3,59 +3,66 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class FindMaximumTest {
-    FindMaximum maximum = new FindMaximum();
+    FindMaximum maximum;
     //  T.C 1.1: TEST CASE TO ENSURE MAX NUMBER IS IN THE FIRST POSITION
     @Test
     public void givenMaximumIntegerNumber_WhenAtFirstPosition_ShouldReturnSameNumber() {
-        Integer findMaxInteger = maximum.maximumValue(new Integer[]{33, 3, 4});
+//        maximum = new FindMaximum();
+        Integer findMaxInteger = (Integer) maximum.testMaximum(33, 3, 4);
         Assert.assertEquals(33, (int) findMaxInteger);
     }
     // T.C 1.2: TEST CASE TO ENSURE MAX NUMBER IS IN THE SECOND POSITION
     @Test
     public void givenMaximumIntegerNumber_WhenAtSecondPosition_ShouldReturnSameNumber() {
-        Integer findMaxInteger = maximum.maximumValue(new Integer[]{3, 33, 4});
+        Integer findMaxInteger = (Integer) maximum.testMaximum(3, 33, 4);
         Assert.assertEquals(33, (int) findMaxInteger);
     }
     // T.C 1.3: TEST CASE TO ENSURE MAX NUMBER IS IN THE THIRD POSITION
     @Test
     public void givenMaximumIntegerNumber_WhenAtThirdPosition_ShouldReturnSameNumber() {
-        Integer findMaxInteger = maximum.maximumValue(new Integer[]{3, 4, 33});
+        Integer findMaxInteger = (Integer) maximum.testMaximum(3, 4, 33);
         Assert.assertEquals(33, (int) findMaxInteger);
     }
     // T.C 2.1 TEST CASE TO ENSURE MAX FLOAT NUMBER IS IN THE FIRST POSITION
     @Test
     public void givenMaximumFloatNumber_WhenAtFirstPosition_ShouldReturnSameNumber() {
-        Float findMaxFloatNumber = maximum.maximumValue(new Float[]{3.1f, 1.1f, 2.1f});
+        Float findMaxFloatNumber = (Float) maximum.testMaximum(3.1f, 1.1f, 2.1f);
         Assert.assertEquals((Float) 3.1f, findMaxFloatNumber);
     }
     // T.C 2.2 TEST CASE TO ENSURE MAX FLOAT NUMBER IS IN THE SECOND POSITION
     @Test
     public void givenMaximumFloatNumber_WhenAtSecondPosition_ShouldReturnSameNumber() {
-        Float findMaxFloatNumber = maximum.maximumValue(new Float[]{1.1f, 3.1f, 2.1f});
+        Float findMaxFloatNumber = (Float) maximum.testMaximum(1.1f, 3.1f, 2.1f);
         Assert.assertEquals((Float) 3.1f, findMaxFloatNumber);
     }
     // T.C 2.3 TEST CASE TO ENSURE MAX FLOAT NUMBER IS IN THE THIRD POSITION
     @Test
     public void givenMaximumFloatNumber_WhenAtThirdPosition_ShouldReturnSameNumber() {
-        Float findMaxFloatNumber = maximum.maximumValue(new Float[]{1.1f, 2.1f, 3.1f});
+        Float findMaxFloatNumber = (Float) maximum.testMaximum(1.1f, 2.1f, 3.1f);
         Assert.assertEquals((Float) 3.1f, findMaxFloatNumber);
     }
     //  T.C 3.1 TEST CASE TO ENSURE MAX STRING IS IN THE FIRST POSITION
     @Test
     public void givenMaximumString_WhenAtFirstPosition_ShouldReturnSameString() {
-        String findMaxString = maximum.maximumValue(new String[]{"Peach", "Apple", "Banana"});
+        String findMaxString = (String) maximum.testMaximum("Peach", "Apple", "Banana");
         Assert.assertEquals("Peach", findMaxString);
     }
     //  T.C 3.2 TEST CASE TO ENSURE MAX STRING IS IN THE SECOND POSITION
     @Test
     public void givenMaximumString_WhenAtSecondPosition_ShouldReturnSameString() {
-        String findMaxString = maximum.maximumValue(new String[]{"Apple", "Peach", "Banana"});
+        String findMaxString = (String) maximum.testMaximum("Apple", "Peach", "Banana");
         Assert.assertEquals("Peach", findMaxString);
     }
     //  T.C 3.3 TEST CASE TO ENSURE MAX STRING IS IN THE THIRD POSITION
     @Test
     public void givenMaximumString_WhenAtThirdPosition_ShouldReturnSameString() {
-        String findMaxString = maximum.maximumValue(new String[]{"Apple", "Banana", "Peach"});
+        String findMaxString = (String) maximum.testMaximum("Apple", "Banana", "Peach");
         Assert.assertEquals("Peach", findMaxString);
+    }
+    // T.C 1.1-3.3: GENERIC TEST CASE TO ENSURE MAX VALUE
+    @Test
+    public <T> void given3GenericValues_WhenCorrect_ShouldReturnMaximumValue() {
+        T genericMaximum = (T) maximum.testMaximum("Apple", "22", "3");
+        Assert.assertEquals("Apple", genericMaximum);
     }
 }
